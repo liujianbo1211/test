12ed6d02fb953234ef85be50ad92f924868e7c0d
Mr.Liu <253456199@qq.com>
2019-04-18 17:04:36 +0800

    11

--- src/main/java/com/jianbo/springbootmybatis/com/jianbo/test/Hello.java
+++ src/main/java/com/jianbo/springbootmybatis/com/jianbo/test/Hello.java
@@ -10,12 +10,13 @@ public class Hello {
     }
 
     public void test2(){
-		System.out.println(5);
+		
         System.out.println();
 		System.out.println(2);
 		System.out.println(3);
 		System.out.println(4);
-
+		System.out.println(5);
+		System.out.println(6);
         
 
     }
