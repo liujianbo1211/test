575b72599e7802bc61e0ed82d806855adcf81d98
Mr.Liu <253456199@qq.com>
2019-04-18 08:56:41 +0800

    first

---
+++ src/main/java/com/jianbo/springbootmybatis/com/jianbo/test/Hello.java
@@ -0,0 +1,11 @@
+package com.jianbo.springbootmybatis.com.jianbo.test;
+
+import com.sun.org.apache.xpath.internal.SourceTree;
+
+public class Hello {
+    public void test(){
+        System.out.println("hello1");
+        System.out.println("hello2");
+        System.out.println("hello3");
+    }
+}
